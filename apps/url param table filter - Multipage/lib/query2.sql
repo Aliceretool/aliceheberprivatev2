d83d1cb0cb1655780661f2cd654368d77743885e
Alice Heber <alice+balloon@retool.com>
2025-04-02 09:27:46 +0000

    Protecting url param table filter - Multipage

---
+++ apps/url param table filter - Multipage/lib/query2.sql
@@ -0,0 +1,1 @@
+select * from boolean_example;
---
+++ apps/url param table filter - Multipage/lib/triggerTargetQuery.js
@@ -0,0 +1,7 @@
+if (tabbedcontainer1.selectedTabIndexTab == 0) {
+  query1.trigger();
+} else if (tabbedcontainer1.selectedTabIndex == 1) {
+  query2.trigger();
+} else if (tabbedcontainer1.selectTab == 2) {
+  query3.trigger();
+}
