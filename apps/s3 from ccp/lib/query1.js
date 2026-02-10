c4e9926b4fa50750314855245e501905629cdaf6
Alice Heber <alice.heber@gmail.com>
2026-02-10 11:59:04 +0000

    Unprotecting s3 from ccp

--- apps/s3 from ccp/lib/query1.js
+++
@@ -1,7 +0,0 @@
-const url = 'https://aliceheber-demo-s3.s3.eu-west-2.amazonaws.com/Proj2-Attachment_+File+3.pptx'; // must be public
-const response = await fetch(url);
-const arrBuf = await response.arrayBuffer();
-const bytes = new Uint8Array(arrBuf);
-let binary = '';
-for (let i = 0; i < bytes.length; i++) binary += String.fromCharCode(bytes[i]);
-return btoa(binary);
